c2896268f968fbbeb2283c67abd4300175e458df
David Herring <davidherring@Davids-MBP.home>
2023-01-14 10:31:13 -0600

    adding install.js logic

--- Develop/client/src/js/install.js
+++ Develop/client/src/js/install.js
@@ -1,11 +1,24 @@
-const butInstall = document.getElementById('buttonInstall');
+const btnInstall = document.getElementById('buttonInstall');
 
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+window.addEventListener('beforeinstallprompt', (event) => {
+    window.deferredPrompt = event;
+    btnInstall.classList.toggle('hidden',false);
+});
 
 // TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+btnInstall.addEventListener('click', async () => {
+    const Event = window.deferredPrompt;
+    if (!Event) {
+        return;
+    }
+    Event.prompt();
+    window.deferredPrompt = null;
+    btnInstall.classList.toggle('hidden', true);
+});
 
 // TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+window.addEventListener('appinstalled', (event) => {
+    window.deferredPrompt = null;
+});
